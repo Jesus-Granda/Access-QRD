3cc4eb1daec50f3fb259515e96b0c586cde7859b
Jesus-Granda <jesus.granda20@outlook.com>
2026-05-21 13:15:13 -0600

    modificacion en config.example.py cambiado a config.py

--- config.example.py
+++
@@ -1,6 +0,0 @@
-class Config:
-
-    SECRET_KEY = "cambia_esta_clave_secreta"
-
-    # Reemplaza con tu cadena de conexión de MongoDB Atlas
-    MONGO_URI = "mongodb+srv://USUARIO:CONTRASEÑA@cluster.mongodb.net/accesoqr?retryWrites=true&w=majority"
---
+++ config.py
@@ -0,0 +1,5 @@
+class Config:
+
+    SECRET_KEY = "accessqr_secret"
+
+    MONGO_URI = "mongodb+srv://al222411673_db_user:acceso0512@accesoqr.mzgfuac.mongodb.net/accesoqr?retryWrites=true&w=majority&appName=accesoqr"
